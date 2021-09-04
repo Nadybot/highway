84cba17705d536e91374cfe8fcdf194345e7575f
Jens Reidel <adrian@travitia.xyz>
2021-09-04 19:31:57 +0200

    use build-std to optimize binary size

--- src/json.rs
+++ src/json.rs
@@ -1,4 +1,6 @@
 #[cfg(not(feature = "simd"))]
 pub use serde_json::{from_reader, from_slice, from_str, to_string, to_vec, Error, Value};
 #[cfg(feature = "simd")]
-pub use simd_json::{from_reader, from_slice, from_str, to_string, to_vec, Error, OwnedValue as Value};
+pub use simd_json::{
+    from_reader, from_slice, from_str, to_string, to_vec, Error, OwnedValue as Value,
+};
